43e2a888d28e0aca5eeeec8727928f39855bb7d5
Nyx <neelabhroghosh23@gmail.com>
2023-12-28 22:40:41 +0530

    Update _init.py

--- _init.py
+++ _init.py
@@ -13,13 +13,14 @@ mydb = mysql.connector.connect(
 
 cur = mydb.cursor()
 
-#cur.execute("CREATE database militech")
-#cur.execute("use militech")
-#cur.execute("CREATE TABLE inventory(p_id int primary key, name varchar(512), unit_price int, count int);")
+cur.execute("CREATE database militech")
+cur.execute("use militech")
+cur.execute("CREATE TABLE inventory(p_id int primary key, name varchar(512), unit_price int, count int);")
 products = [("Militech M-179 Achilles", 40000), ("Militech M-10AF Lexington", 34000), ("Militech M221 Saratoga",25000) , ("Militech Crusher", 12000), ("Militech M251s Ajax",45000),
             ("Militech Mk. 31 HMG", 31000),("Militech Basilisk", 9000),("Militech Behemoth", 56000), ("Militech Manticore", 23000), ("Militech Berserk Mk.5", 59000)]
-for x in range(11):
+for x in range(10):
   cmd = "INSERT INTO inventory values(" + str(x) + ", \"" + products[x][0] + "\", " + str(products[x][1]) + ", " + "999);"
+  cur.execute(cmd)
 
 cur.execute("CREATE TABLE customers(c_id int primary key, name varchar(512))")
 cur.execute("CREATE TABLE transactions(c_id int, amount int)")
